92786b2c1a3f58269ee3bf1bdb1e2b7468a0658d
Jose Pablo <josepabloruizperez99@gmail.com>
2023-11-23 18:08:43 +0100

    Version 1.0.0-rc lista

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard, ProductImage, ProductButtons, ProductTitle } from '../.';
 
 const product1 = {
   id: '1',
-  title: 'Coffee Mug - Card',
+  title: 'Coffee Mug - Card!',
   // img: './coffee-mug.png'
 }
 
